d8a77c6aec0d812a3ff85218aa92e4e1ab17a057
aliyasser01 <120260364+aliyasser01@users.noreply.github.com>
2022-12-11 00:41:38 +0200

    Add files via upload

---
+++ 2.java
@@ -0,0 +1,37 @@
+// side-bar 
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+}
+// sidebar
+function w3_open() {
+    document.getElementById("main").style.marginRight = "25%";
+    document.getElementById("mySidebar").style.width = "25%";
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("openNav").style.display = 'none';
+}
+function w3_close() {
+    document.getElementById("main").style.marginRight = "0%";
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("openNav").style.display = "inline-block";
+}
+
+
+// video content
+
+
+var video = document.getElementById("myVideo");
+var btn = document.getElementById("myBtn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
